5b0638cfb55a409bd8b2c6386f9f10b61f45619e
Animesh Jain <animesh.jain1203@gmail.com>
2018-08-04 23:23:19 +0530

    autoscroll added

--- app/js/app_new.js
+++ app/js/app_new.js
@@ -16,6 +16,20 @@ app.directive('ngEnter', function () {
         }
     };
 });
+app.directive('scrollBottom', function() {
+	return {
+        restrict: 'A',
+        controller: 'LoginController',
+        controllerAs: 'lc',
+		link: function(scope, element) {
+			scope.$watchCollection('lc.messageList', function() {
+		    var $list = $(element).find('.message-list');
+				var scrollHeight = $list.prop('scrollHeight');
+				$list.animate({scrollTop: scrollHeight}, 500);			    
+			});
+		}
+	};
+});
 app.controller('LoginController', ['$scope', function ($scope) {
     var lc = this;
 
@@ -25,7 +39,39 @@ app.controller('LoginController', ['$scope', function ($scope) {
     }, {
         text: "Hello I am Animesh. PLease book a doctor appointment",
         person: true
-    }, ];
+    }, {
+        text: "Ok tell me the date",
+        person: false
+    }, {
+        text: "I want to book for next wednesday",
+        person: true
+    }, {
+        text: "Let me check if any doctor is available",
+        person: false
+    }, {
+        text: "I am unable to find any availability. Do you want to try other days?",
+        person: false
+    }, {
+        text: "Sure.",
+        person: true
+        }
+//    , {
+//        text: "There is availability for next Friday. Should I book the appointment?",
+//        person: false
+//    }, {
+//        text: "Ok",
+//        person: true
+//    }, {
+//        text: "Your appointment is booked for Friday 10:00AM at ABC Hospital.",
+//        person: false
+//    }, {
+//        text: "Thank You",
+//        person: true
+//    }, {
+//        text: "Your Welcome, Animesh!",
+//        person: false
+//    }
+                     ];
 
     lc.send = function () {
         if (lc.inputMsg && lc.inputMsg !== '') {
@@ -33,6 +79,8 @@ app.controller('LoginController', ['$scope', function ($scope) {
                 text: lc.inputMsg,
                 person: true
             });
+
+            lc.inputMsg = '';
         }
     };
 
