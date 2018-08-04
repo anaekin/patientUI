3df1a2989db0ee4b25798e074467ab58a657b2b8
Animesh Jain <animesh.jain1203@gmail.com>
2018-08-04 22:00:51 +0530

    added voice recog button and enter directives

--- app/js/app_new.js
+++ app/js/app_new.js
@@ -1,23 +1,39 @@
 var app = angular.module('NapierAssistant', []);
-
-app.controller('LoginController' ,['$scope' ,function($scope){
+app.directive('ngEnter', function () {
+    return {
+        restrict: 'A',
+        controller: 'LoginController',
+        controllerAs: 'lc',
+        link: function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function () {
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        }
+    };
+});
+app.controller('LoginController', ['$scope', function ($scope) {
     var lc = this;
-    
+
     lc.messageList = [{
-        text : "Hi! How Can I help you? Please write or say something",
-        person : false
-    },{
-        text : "Hello I am Animesh. PLease book a doctor appointment",
-        person : true
-    },];
-    
-    lc.send = function(){
-        if(lc.inputMsg && lc.inputMsg !== ''){
+        text: "Hi! How Can I help you? Please write or say something",
+        person: false
+    }, {
+        text: "Hello I am Animesh. PLease book a doctor appointment",
+        person: true
+    }, ];
+
+    lc.send = function () {
+        if (lc.inputMsg && lc.inputMsg !== '') {
             lc.messageList.push({
-                text : lc.inputMsg,
+                text: lc.inputMsg,
                 person: true
             });
         }
     };
-    
+
 }]);
